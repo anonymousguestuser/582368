11ece715f85e979a7be0620f2f2fd7b7ad7292f9
Guest user <root@carrata.net>
2014-01-16 13:20:03 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ratebeer::Application.config.secret_key_base = '1f63cd8d9ddc637f455146dca6ac62e4043446db3f5917e3ee46f486e6f5f0106d40717bb27e6fd02bad284e576f1bc98e186065828674cb95a39e86c8e32626'
