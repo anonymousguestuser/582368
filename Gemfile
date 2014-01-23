c584b3dde1616225d5494cca0b2030b8cc3e999e
Guest user <root@carrata.net>
2014-01-23 02:30:38 +0200

    kierros 2, tehtävään 6 saakka tehty

--- Gemfile
+++ Gemfile
@@ -54,3 +54,5 @@ group :production do
    gem 'pg'
    gem 'rails_12factor' 
 end
+
+gem 'byebug', group: [:development, :test]
--- app/models/beer.rb
+++ app/models/beer.rb
@@ -1,3 +1,16 @@
 class Beer < ActiveRecord::Base
   belongs_to :brewery
+  has_many :ratings
+
+  def average_rating
+    #avg = 0.0
+    #ratings.each{ |r| avg += r.score}
+    #(avg / ratings.count).round(2)
+
+    #(ratings.inject(0.0) {|sum,item| sum + item.score} / ratings.count).round(2)
+
+    ratings.average('score').round(2)
+
+  end
+
 end
