46ce40d8346512a8387158fa9855dedbd6050f81
Guest user <root@carrata.net>
2014-01-25 17:00:23 +0200

    Tehtävä 14

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,4 +1,9 @@
 class Brewery < ActiveRecord::Base
   has_many :beers, :dependent => :destroy
+  has_many :ratings, :through => :beers
+
+  def average_rating
+    self.ratings.average('score').round(2)
+  end
 
 end
