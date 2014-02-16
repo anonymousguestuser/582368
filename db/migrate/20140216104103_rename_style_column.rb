ab262744bf40c2a72dbd899843b2976db0f3c042
Guest user <root@carrata.net>
2014-02-16 13:48:37 +0200

    Removed typo

--- db/migrate/20140216104103_rename_style_column.rb
+++ db/migrate/20140216104103_rename_style_column.rb
@@ -1,6 +1,6 @@
 class RenameStyleColumn < ActiveRecord::Migration
   def change
     rename_column :beers, :style, :old_style
-    add_reference :styles, index:truerails
+    add_reference :styles, index:true
   end
 end
